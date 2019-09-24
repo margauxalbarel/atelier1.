0df7c0f87c02f4620bbdbbaa52fd7c5c667f7ec5
margauxalbarel <55747480+margauxalbarel@users.noreply.github.com>
2019-09-24 15:58:34 +0200

    Add files via upload

---
+++ biostat.R
@@ -0,0 +1,33 @@
+1+2
+sort(c(2,5,1,3))
+2*(3+2
+X<-2
+x <- 2   
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+taillestailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+length(tailles)
+mean(tailles) #MOYENNE
+var(tailles)  #VARIANCE
+sd(tailles)  #ECART TYPE
+summary(tailles)
+hist(tailles)
+taleS.m <- tailles/100
+sin(tailles)
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+tailles.m <- tailles/100
+imc <- poids/(tailles.m^2)
+imc
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+tailles.m <- tailles/100
+imc <- poids/(tailles.m^2)
+imc
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+sd(poids)
+summary(poids)
+imc <- poids/tailleS.m^2
+imc
+donnees <- data.frame(tailles,poids,imc)
